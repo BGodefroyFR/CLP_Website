a430d24f77d3eb04fce9a4f370a455e2b2b25b9d
BGodefroyFR <bruno.godefroy@insa-lyon.fr>
2017-02-06 23:56:05 +0100

    Added content

---
+++ assets/perso.js
@@ -0,0 +1,15 @@
+$( document ).ready(function() {
+
+  // Scroll to section
+  $('*[href*="#"]:not([href="#"])').click(function() {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 700);
+      return false;
+    }
+  });
+
+});
--- index.php
+++ index.php
@@ -12,6 +12,8 @@
 <!-- font awesome -->
 <link href="assets/font-awesome.min.css" rel="stylesheet">
 
+<link href="assets/perso.css" rel="stylesheet">
+
 <!-- bootstrap -->
 <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
 
@@ -95,10 +97,12 @@
         </figcaption>
     </figure>
     <figure class="effect-zoe">
+        <a href="#about">
         <img src="images/tmp/logo3.png" alt="img01"/>
         <figcaption>
             <h2>La&nbsp;&nbsp; compagnie</h2>           
         </figcaption>
+        </a>
     </figure>
      <figure class="effect-zoe">
         <img src="images/tmp/3_menu.jpg" alt="img01"/>
@@ -130,28 +134,30 @@
 
 
 
-<!-- Cirlce Starts -->
-<div id="about"  class="container spacer about">
-<h2 class="text-center wowload fadeInUp" style="font-weight: 100; font-size: 37px;">Compagnie le Passage</h2>  
-  <div class="row">
-    <div class="col-sm-6">
-      <div class="wowload fadeInLeft">
-        <p>La compagnie Le Passage est installée en Charente depuis 2009.<br />
-        Elle est portée par l'association Scènes en Chantier.</p>
-      </div><br />
-      <div class="wowload fadeInLeft">
-      <h5>Mentions légales</h5>
-      <p>Scènes en chantier : Association loi 1901.<br />
-        Association dépendante de la mairie de Barret (16).<br />
-        N° de licence de spectacle : 2-1036850<br /><br />
-        Président et responsable de la publication :<br />
-        Gerard Pelletant<br />
-        7, cité de l’Etang - 16120 Chateauneuf sur Charente<br />
-        Tél : 06 14 05 30 57<br />
-        paps@club-internet.fr</p>    
+<section id="about" style="background-color: #f1f1f1;">
+  <div class="container spacer about">
+    <h2 class="text-center wowload fadeInUp" style="font-weight: 100; font-size: 37px;">Compagnie le Passage</h2>  
+    <div class="row">
+      <div class="col-sm-6">
+        <div class="wowload fadeInLeft">
+          <p>La compagnie Le Passage est installée en Charente depuis 2009.<br />
+          Elle est portée par l'association Scènes en Chantier.</p>
+        </div><br />
+        <div class="wowload fadeInLeft">
+        <h5>Mentions légales</h5>
+        <p>Scènes en chantier : Association loi 1901.<br />
+          Association dépendante de la mairie de Barret (16).<br />
+          N° de licence de spectacle : 2-1036850<br /><br />
+          Président et responsable de la publication :<br />
+          Gerard Pelletant<br />
+          7, cité de l’Etang - 16120 Chateauneuf sur Charente<br />
+          Tél : 06 14 05 30 57<br />
+          paps@club-internet.fr</p>    
+        </div>
       </div>
     </div>
-  </div>
+  <div>
+</section>
 
 <!--Contact Starts-->
 <div id="contact" class="spacer">
@@ -178,8 +184,8 @@
 
 <!-- Footer Starts -->
 <div class="footer text-center spacer">
-<p class="wowload flipInX"><a href="https://www.facebook.com/lepassage.fr"><i class="fa fa-facebook fa-2x"></i></a></p>
-Compagnie le Passage<br/>2009 - <?php echo date("Y")?>
+<p class="wowload flipInX"><a target="_blank" href="https://www.facebook.com/lepassage.fr"><i class="fa fa-facebook fa-2x"></i></a></p>
+Compagnie le Passage <br /><br /> 2009 - <?php echo date("Y"); ?>
 </div>
 <!-- # Footer Ends -->
 <a href="#works" class="gototop "><i class="fa fa-angle-up  fa-3x"></i></a>
@@ -204,6 +210,8 @@ Compagnie le Passage<br/>2009 - <?php echo date("Y")?>
 
 <!-- jquery -->
 <script src="assets/jquery.js"></script>
+
+<script src="assets/perso.js"></script>
 
 <!-- wow script -->
 <script src="assets/wow/wow.min.js"></script>
