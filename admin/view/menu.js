c3a3b3f08b96be02b12e5660f7c9ea87ae401b16
BGodefroyFR <godefroy@kth.se>
2017-03-25 14:27:22 +0100

    Added actions to admin menu

--- admin/view/menu.js
+++ admin/view/menu.js
@@ -20,6 +20,19 @@ $( document ).ready(function() {
 		     $('#sections').append(data);
 		});
 	});
+
+	// Actions
+	$(document).on("click", ".content > .contentElem .actions .up",function() {
+		if ($(this).parent().parent().prev().hasClass('contentElem'))
+			$(this).parent().parent().swapWith( $(this).parent().parent().prev() );
+	});
+	$(document).on("click", ".content > .contentElem .down",function() {
+		if ($(this).parent().parent().next().hasClass('contentElem'))
+			$(this).parent().parent().swapWith( $(this).parent().parent().next() );
+	});
+	$(document).on("click", ".content > .contentElem .delete",function() {
+		$(this).parent().parent().remove();
+	});
 	
 
 
